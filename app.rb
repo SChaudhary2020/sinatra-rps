c1829913c26da53352e4b24d5e97378b8c910a9c
Sarita Chaudhary <71405330+SChaudhary2020@users.noreply.github.com>
2024-12-29 22:49:41 +0000

    Sinatra app2

--- app.rb
+++ app.rb
@@ -15,12 +15,34 @@ elsif @comp_move == "paper"
 else 
   @outcome = "won"
 end
-
-
 erb(:zebra)
-
 end
 
 get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "paper"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @coutcome = "lost"
+else 
+  @outcome = "won"
+end
   erb(:giraffe)
+end
+
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "scissors"
+  @outcome = "tied"
+elsif @comp_move == "scissors"
+  @coutcome = "lost"
+else 
+  @outcome = "won"
+end
+  erb(:horse)
 end
